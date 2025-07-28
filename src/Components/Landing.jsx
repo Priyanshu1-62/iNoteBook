3e5dfd14fb185f09eb1d7b05fcc89c80d9a0e2e4
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-07-28 20:46:20 +0530

    Cleanup

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -29,7 +29,7 @@ function Landing() {
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-stone-800 mb-7">iNoteBook - your second brain</h1>
         <p className="text-base md:text-lg mb-2">- Save your insights, Eureka moments and hot teas anytime, anywhere, from any device. </p>
         <p className="text-base md:text-lg mb-2">- Explore dark mode, create Notes super easily, and manage them with visual ease. </p>
-        <p className="text-base md:text-lg mb-2">- For project infrastructure details or to report a bug, check out the Feedback section.. </p>
+        <p className="text-base md:text-lg mb-2">- For project infrastructure details or to report a bug, check out the Feedback section. </p>
         <div className="flex flex-row-reverse mt-3">
           <button disabled={loadingLanding} className={`flex px-3 py-2 md:px-3 md:py-2 w-fit bg-[#151833] text-white font-bold border-4 border-cyan-900 rounded-xl ${loadingLanding?"opacity-50 cursor-not-allowed":"opacity-100 hover:bg-[#2c2f4a] active:bg-[#1e2351] cursor-pointer"}`} onClick={handlestart}>
             <p className="text-sm md:text-base">Get Started &nbsp;</p>
